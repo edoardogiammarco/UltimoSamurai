b9dd3e44b5bb4d2cde271f18a193a945e709f4de
edoardogiammarco <edoardo.giammarco@gmail.com>
2022-12-06 11:10:35 +0100

    Joystick, Player Movement
    Added fully functioning joystick, linked to player movement.
    
    TO DO: camera follows player movement, create tile map, collisions with broder of map.

---
+++ UltimoSamurai/Assets/Player.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private BoxCollider2D boxCollider;
+    private Vector3 moveDelta;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();    
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        // Reset MoveDelta
+        moveDelta = Vector3.zero;
+
+        float x = Input.GetAxisRaw("Horizontal");
+    }
+}
