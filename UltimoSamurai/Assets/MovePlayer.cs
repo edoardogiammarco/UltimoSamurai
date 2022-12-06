46ef8a6de5b58d26d31972475141c8d2c9f197eb
Dip <damianodipadova@gmail.com>
2022-12-06 12:43:09 +0100

    Fixed Movement
    Fixed movement and added sprite flip

--- UltimoSamurai/Assets/MovePlayer.cs
+++ UltimoSamurai/Assets/MovePlayer.cs
@@ -8,6 +8,7 @@
         public Joystick movementJoystick;
         public float playerSpeed;
         private Rigidbody2D rb;
+      //  private int playerscale;
     
         // Start is called before the first frame update
         void Start()
@@ -18,9 +19,17 @@
         // Update is called once per frame
         void FixedUpdate()
         {
-            if(movementJoystick.joystickVec.y != 0)
+            if(movementJoystick.joystickVec.y != 0 || movementJoystick.joystickVec.x != 0)
             {
+                if( movementJoystick.joystickVec.x>0){
+                gameObject.transform.localScale = new Vector2(1,1);
                 rb.velocity = new Vector2(movementJoystick.joystickVec.x * playerSpeed, movementJoystick.joystickVec.y * playerSpeed);
+                }
+                else {
+                    gameObject.transform.localScale = new Vector2(-1,1);
+                    rb.velocity = new Vector2(movementJoystick.joystickVec.x * playerSpeed, movementJoystick.joystickVec.y * playerSpeed);
+                    
+                }
             }
             else
             {
