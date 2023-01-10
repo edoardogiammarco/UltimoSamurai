eac3a0c373f1d53e3dabac4d2d8c9da9057f70a7
edoardogiammarco <edoardo.giammarco@gmail.com>
2023-01-10 17:30:35 +0100

    Edited PlayerCombat code
    Attacking animations will not overlap each other

--- UltimoSamurai/Assets/PlayerCombat.cs
+++ UltimoSamurai/Assets/PlayerCombat.cs
@@ -12,28 +12,38 @@ public class PlayerCombat : MonoBehaviour
     // Update is called once per frame
    public void Update()
     {
-        if( Input.GetButtonDown("Attack1Button")){
-            
+
+        isAttacking = AnimatorIsPlaying();
+        if( Input.GetButtonDown("Attack1Button")) {
             Attack1();
         }
     
-        if( Input.GetButtonDown("Attack2Button")){
+        if( Input.GetButtonDown("Attack2Button")) {
             Attack2();
         }    
         
     }
 
-    public void Attack1(){
-        /*if ( !isAttacking)*/  animator.SetTrigger("Attack1");
-        
+    public void Attack1() {
 
-    }
-    public void Attack2(){
-        /*if(!isAttacking)*/  animator.SetTrigger("Attack2");
-        
+        if (!isAttacking)animator.SetTrigger("Attack1");
 
     }
 
+    public void Attack2() {
+
+        if(!isAttacking)    animator.SetTrigger("Attack2");        
+
+    }
+
+    // Checks if player is currently attacking
+    bool AnimatorIsPlaying() {
+
+        return AnimatorIsPlaying()
+                && animator.GetCurrentAnimatorStateInfo(0).IsName("Attack1") 
+                && animator.GetCurrentAnimatorStateInfo(0).IsName("Attack2");
+    
+    }
  
 
 
