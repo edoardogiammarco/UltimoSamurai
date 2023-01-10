3af19bacd635ec9446907b7c668eb3de9e33606e
Dip <damianodipadova@gmail.com>
2023-01-10 16:10:00 +0100

    Fixed Attack animation
    //

--- UltimoSamurai/Assets/PlayerCombat.cs
+++ UltimoSamurai/Assets/PlayerCombat.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+    public Animator animator;
+    public bool isAttacking = false;
+
+    // Update is called once per frame
+   public void Update()
     {
+        if( Input.GetButtonDown("Attack1Button")){
+            
+            Attack1();
+        }
+    
+        if( Input.GetButtonDown("Attack2Button")){
+            Attack2();
+        }    
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    public void Attack1(){
+        /*if ( !isAttacking)*/  animator.SetTrigger("Attack1");
         
+
     }
+    public void Attack2(){
+        /*if(!isAttacking)*/  animator.SetTrigger("Attack2");
+        
+
+    }
+
+ 
+
+
+
+
 }
