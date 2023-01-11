06b4f9a74742b5133260792a0f9f6dd264c81928
edoardogiammarco <edoardo.giammarco@gmail.com>
2023-01-11 16:59:55 +0100

    Added basic AI for baseEnemy
    -

--- UltimoSamurai/Assets/Script/BaseEnemyScript.cs
+++ UltimoSamurai/Assets/Script/BaseEnemyScript.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class BaseEnemyScript : MonoBehaviour
 {
 
+    public Transform player;
+    //da modificare
+    int moveSpeed = 4;
+    int maxDist = 10;
+    int minDist = 5;
     public int maxHealt = 100;
     int currentHealth;
     public Animator animator;
@@ -37,11 +44,22 @@ public class BaseEnemyScript : MonoBehaviour
         GameObject.Destroy(enemy,2f);
     }
 
-
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(Player);
+ 
+         if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+         {
+ 
+             transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+
+             if (Vector3.Distance(transform.position, Player.position) <= MaxDist)
+             {
+                 //Here Call any function U want Like Shoot at here or somet$$anonymous$$ng
+             }
+         }
     }
 }
