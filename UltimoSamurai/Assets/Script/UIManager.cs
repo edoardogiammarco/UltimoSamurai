abba16e36af89a869a31e3a0195028e39f97edc7
edoardogiammarco <edoardo.giammarco@gmail.com>
2023-02-20 11:30:50 +0100

    Revert "Leaderboard - 1"
    This reverts commit e1bbf95d1389ce07ded66d6938ba6aaf33bf0b2f.

--- UltimoSamurai/Assets/Script/UIManager.cs
+++ UltimoSamurai/Assets/Script/UIManager.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class UIManager : MonoBehaviour
 {
@@ -10,8 +9,6 @@ public class UIManager : MonoBehaviour
     //Screen object variables
     public GameObject loginUI;
     public GameObject registerUI;
-    public GameObject userDataUI;
-    public GameObject scoreboardUI;
 
     private void Awake()
     {
@@ -27,35 +24,14 @@ public class UIManager : MonoBehaviour
     }
 
     //Functions to change the login screen UI
-
-    public void ClearScreen() //Turn off all screens
-    {
-        loginUI.SetActive(false);
-        registerUI.SetActive(false);
-        userDataUI.SetActive(false);
-        scoreboardUI.SetActive(false);
-    }
-
     public void LoginScreen() //Back button
     {
-        ClearScreen();
         loginUI.SetActive(true);
+        registerUI.SetActive(false);
     }
     public void RegisterScreen() // Regester button
     {
-        ClearScreen();
+        loginUI.SetActive(false);
         registerUI.SetActive(true);
-    }
-
-    public void UserDataScreen() //Logged in
-    {
-        ClearScreen();
-        userDataUI.SetActive(true);
-    }
-
-    public void ScoreboardScreen() //Scoreboard button
-    {
-        ClearScreen();
-        scoreboardUI.SetActive(true);
     }
 }
